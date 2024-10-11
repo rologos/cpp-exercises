16019b71175b0a62532b74fd89267cb7d2544b2e
rologos <170283129+rologos@users.noreply.github.com>
2024-10-10 19:54:00 -0500

    add pascal triangle fiels

---
+++ pascalstriangle/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+void generate_triangle() {
+    std::vector<int> data{ 1 };
+
+    for (auto number : data) {
+        std::cout << number << ' ';
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    generate_triangle();
+}
