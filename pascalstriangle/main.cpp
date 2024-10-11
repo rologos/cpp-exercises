1f7d638871287e08bae804424f1b0ce54e33783c
rologos <170283129+rologos@users.noreply.github.com>
2024-10-10 20:15:27 -0500

    add getnextrow method to build non-trivial triangle

--- pascalstriangle/main.cpp
+++ pascalstriangle/main.cpp
@@ -10,6 +10,17 @@ void generate_triangle() {
     std::cout << '\n';
 }
 
+std::vector<int> get_next_row(const std::vector<int> & last_row) {
+    std::vector<int> next_row { 1 };
+    if (last_row.empty()) {
+        return next_row;
+    }
+    for (size_t idx = 0; idx + 1 < last_row.size(); ++idx) {
+        next_row.emplace_back(last_row[idx] + last_row[idx + 1]);
+    }
+    next_row.emplace_back(1);
+    return next_row;
+
 int main() {
     generate_triangle();
 }
