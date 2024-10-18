211d059ec7db0e023197f3bd342eb87ea04a9321
rologos <170283129+rologos@users.noreply.github.com>
2024-10-18 15:25:32 -0500

    read into string rather than unsigned to accept negative numbers option for user to input a string to give up

--- guessinggame/main.cpp
+++ guessinggame/main.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <optional>
 
 unsigned some_const_number() {
     return 42;
 }
 
-unsigned input () {
-    unsigned number;
-    while (!(std::cin >> number)) {
-        std::cin.clear();
-        std::cin.ignore(
-            std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Please enter a number.\n>";
+std::optional<int> read_number(std::istream& in) {
+    int result{};
+    if (in >> result) {
+        return result;
     }
-    return number;
+    in.clear();
+    in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return {};
 }
 
-void guess_number (unsigned number) {
+void guess_number_or_give_up (int number) {
     std::cout << "Guess the number.\n>";
-    unsigned guess = input();
-    while (guess != number) {
-        std::cout << guess <<" is wrong. Try again\n>";
-        guess = input();
+    std::optional<int> guess;
+    while ((guess = read_number(std::cin))) {
+
+        if (guess.value() == number) {
+            std::cout << "Well done.";
+            return;
+        }
+        std::cout << guess.value() <<" is wrong. Try again\n>";
     }
-    std::cout << "Well done.\n>";
+    std::cout << "The number was " << number << "\n>";
 }
 
 int main() {
-    guess_number( some_const_number());
+    guess_number_or_give_up( some_const_number());
 }
