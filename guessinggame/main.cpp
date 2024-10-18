ddf7483566d1f19e8c4ac8d78a93c71ffe209f50
rologos <170283129+rologos@users.noreply.github.com>
2024-10-17 21:28:58 -0500

    ask for input from user

--- guessinggame/main.cpp
+++ guessinggame/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+unsigned some_const_number() {
+    return 42;
+}
+
+unsigned input () {
+    unsigned number;
+    while (!(std::cin >> number)) {
+        std::cin.clear();
+        std::cin.ignore(
+            std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Please enter a number.\n>";
+    }
+}
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
