08fd2f1d9d536bd2587c1dad373d151e12802886
rologos <170283129+rologos@users.noreply.github.com>
2025-03-04 21:55:20 -0600

    update to take argument form cli

--- absvalue/absValue.cpp
+++ absvalue/absValue.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
+#include <stdlib.h>
 
 int absolute_value (int x){
     if (x >= 0) return x;
     else return -x;
 }
 
-int main () {
-    int my_num = -10;
+int main (int argc, char *argv[]) {
+    int my_num = atoi(argv[1]);
     printf( "The absolute value of %d is %d.\n", my_num, absolute_value(my_num));
 }
