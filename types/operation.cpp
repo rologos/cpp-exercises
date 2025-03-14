b75a10748a205cf23aa750de366fdfaea9257ddc
rologos <170283129+rologos@users.noreply.github.com>
2025-03-14 16:11:31 -0500

    calculate numbers based on enum passed to constructor

--- types/operation.cpp
+++ types/operation.cpp
@@ -11,40 +11,55 @@ enum class operation {
 
 struct calculator {
 
-operation op;
+    operation op;
 
-calculator(operation operationValue) : op (operationValue) {
-};
-
-void setOperation(operation operationValue) {
-    op = operationValue;
-};
-
-operation getOperation() { return op; };
-
-void printColor() const {
-    switch (op) {
-        case operation::Add:
-            std::cout << "Color: Add" << std::endl;
-            break;
-        case operation::Subtract:
-            std::cout << "Color: Subtract" << std::endl;
-            break;
-        case operation::Multiply:
-            std::cout << "Color: Multiply" << std::endl;
-            break;
-        default:
-            std::cout << "Color: Divide" << std::endl;
-            break;
+    calculator(operation operationValue) : op (operationValue) {
     };
-};
+
+    void setOperation(operation operationValue) {
+        op = operationValue;
+    };
+
+    operation getOperation() { return op; };
+
+    void printEnum() const {
+        switch (op) {
+            case operation::Add:
+                std::cout << "Color: Add" << std::endl;
+                break;
+            case operation::Subtract:
+                std::cout << "Color: Subtract" << std::endl;
+                break;
+            case operation::Multiply:
+                std::cout << "Color: Multiply" << std::endl;
+                break;
+            default:
+                std::cout << "Color: Divide" << std::endl;
+                break;
+        };
+    };
+
+    int calculate (int x, int y) {
+        switch (op) {
+            case operation::Add:
+                return x + y;
+                break;
+            case operation::Subtract:
+                return x - y;
+                break;
+            case operation::Multiply:
+                return x * y;
+                break;
+            default:
+                return x / y;
+                break;
+        };
+    }
 };
 
 int main() {
 
     calculator myCalculator = operation::Divide;
-    myCalculator.printColor();
-    myCalculator.setOperation(operation::Multiply);
-    myCalculator.printColor();
-    printf("This is an enum: %d \n", static_cast<int>(myCalculator.getOperation()));
+    myCalculator.printEnum();
+    printf("This is the result of the calc: %d \n", myCalculator.calculate(14,2));
 };
